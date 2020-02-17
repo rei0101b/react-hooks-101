9d49f19d5bd4341a14872bb7a4b65ae7cf9b4d40
rei0101b <rei0101b@gmail.com>
2020-02-17 17:48:56 +0900

    @@

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 
 export const App = props => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  const [state, setState] = useState(props);
+  const { name, price } = state;
+
   return (
     <>
       <div>
         <p>
           現在の{name}は、{price}円です
         </p>
-        <button onClick={() => setPrice(price + 1)}>＋</button>
-        <button onClick={() => setPrice(price - 1)}>ー</button>
-        <button onClick={reset}>R</button>
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <button onClick={() => setState({ ...state, price: price + 1 })}>
+          ＋
+        </button>
+        <button onClick={() => setState({ ...state, price: price - 1 })}>
+          ー
+        </button>
+        <button onClick={() => setState(props)}>R</button>
+        <input
+          value={state.name}
+          onChange={e => setState({ ...state, name: e.target.value })}
+        />
       </div>
     </>
   );
